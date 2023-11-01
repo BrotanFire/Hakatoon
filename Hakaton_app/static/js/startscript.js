6452667ab76423c560945748e177202d3469fc3f
BroFire <User@BROFIRE>
2023-11-01 14:11:46 +0500

    Add some func

--- Hakaton_app/static/js/startscript.js
+++ Hakaton_app/static/js/startscript.js
@@ -15,7 +15,7 @@ async function GetToken()
 const json = await response.json();
 console.log("Успех:", JSON.stringify(json));
 console.log(json.token)
-document.cookie = 'token='+json.token
+document.cookie = "token="+json.token+"; SameSite=None; Secure"
 ShowAllTasks()
 }
 function getCookie(name) {
@@ -36,5 +36,70 @@ async function ShowAllTasks()
   },
 });
 let TasksData = await response.json();
-console.log(TasksData)
+let Table_body = document.getElementById("row_start")
+Table_body.innerHTML = '';
+for (let i in TasksData)
+		{
+			let Cell = document.createElement("div");
+			Cell.className = "col";
+			Cell.setAttribute("id",i)
+			Table_body.appendChild(Cell);
+			let cell = document.createElement("div")
+			cell.className = "card mb-4 shadow-sm h-80"
+			let cell_header = document.createElement("div")
+			cell_header.className = "card-header"
+			let cell_title = document.createElement("h4")
+			cell_title.className = "my-0 font-weight-normal"
+			cell_title.innerHTML = TasksData[i].title
+			let cell_body = document.createElement("div")
+			cell_body.className = "card-body"
+			let body_text =  document.createElement('ul');
+			body_text.className = "list-unstyled"
+			let body_text_description =  document.createElement("h6");
+			body_text_description.innerHTML = 'Описание: ' + TasksData[i].description
+			let body_text_status =  document.createElement("h6");
+			body_text_status.innerHTML = 'Статус: ' + TasksData[i]['status'].status_ru
+			let div = document.createElement('div');
+            div.className = "alert";
+            div.innerHTML = '<button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#TaskInfo" onclick = "ShowTask('+TasksData[i].id+')">Подробнее</button>';
+			Cell.appendChild(div);
+			Cell.appendChild(cell)
+			cell.appendChild(cell_header)
+			cell.appendChild(cell_body)
+			cell_header.appendChild(cell_title)
+			cell_body.appendChild(body_text)
+			body_text.appendChild(body_text_description)
+			body_text.appendChild(body_text_status)
+			cell.appendChild(div)
+		}
+		console.log(TasksData)
+}
+
+async function ShowTask(TaskId)
+{
+var token = getCookie('token');
+
+ let response = await fetch('http://92.51.44.167:8080/task/'+TaskId, {
+  method: 'GET',
+  headers: {
+    'Authorization': 'Token '+token ,
+    'Content-Type': 'application/json'
+  },
+});
+let TaskData  = await response.json();
+console.log(TaskData)
+let title = document.getElementById('title')
+let EmpName = document.getElementById('EmpName')
+let Status = document.getElementById('Status')
+let Desc = document.getElementById('Desc')
+let created_ts = document.getElementById('created_ts')
+let in_progress_ts = document.getElementById('in_progress_ts')
+let executed_ts = document.getElementById('executed_ts')
+title.innerHTML = 'Задача: ' + TaskData[0].title
+Desc.innerHTML = 'Подробно: ' + TaskData[0].description
+EmpName.innerHTML = 'ФИО: ' +  TaskData[0].assigned_to.first_name +' '+ TaskData[0].assigned_to.second_name
+Status.innerHTML = 'Статус: ' + TaskData[0].status.status_ru
+created_ts.innerHTML = 'Задача создана: ' + dateformat(TaskData[0].created_ts, 'DD.MM.YYYY')
+in_progress_ts.innerHTML = 'Задача принята в работу: ' + TaskData[0].in_progress_ts
+executed_ts.innerHTML = 'Задача выполнена: ' + TaskData[0].executed_ts
 }
