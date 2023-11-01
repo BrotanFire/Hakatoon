ac2d3580e3055aa101396b8d4a16506f247412e0
BroFire <User@BROFIRE>
2023-11-01 11:19:08 +0500

    add token save cockie

--- Hakaton_app/static/js/startscript.js
+++ Hakaton_app/static/js/startscript.js
@@ -17,3 +17,15 @@ console.log("Успех:", JSON.stringify(json));
 console.log(json.token)
 document.cookie = 'token='+json.token
 }
+
+async function ShowAllTasks()
+{
+ let response = await fetch('http://92.51.44.167:8080/api/token', {
+  method: 'GET',
+  headers: {
+    'Authorization': 'Token ad7be1d2088cd1490b5b6f6597080e14df6f404a'
+    'Content-Type': 'application/json'
+  },
+});
+console.log(response.json)
+}
