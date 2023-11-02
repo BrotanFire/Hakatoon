90fad26b3d849348b48217485a6b40fef22256e0
Drovosyaka <grif.k@mail.ru>
2023-11-02 11:57:08 +0500

    123

--- Hakatoon/Hakaton_app/static/js/index.js
+++ Hakatoon/Hakaton_app/static/js/index.js
@@ -1,3 +1,3 @@
-var difficulty = 1; // Задайте желаемую сложность (любое целое число)
+var difficulty = 3; // Задайте желаемую сложность (любое целое число)
 var progressBar = document.querySelector('.progress-bar');
 progressBar.classList.add('difficulty-' + difficulty);
